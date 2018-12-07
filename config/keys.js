cddcc80ebc79a2d24d7d3fa89c41fcf4bb8cacf4
debasish283 <debasishmisra3008@gmail.com>
2018-12-07 11:45:10 +0530

    test

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//keys.js
+if (process.env.NODE_ENV === "production") {
+  //production set of keys
+  module.exports = require("./prod");
+} else {
+  //development set of keys
+  module.exports = require("./dev");
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+var passport = require("passport");
+require("./models/User");
+require("./services/passport.js");
+
+mongoose.connect(keys.mongoUri);
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
